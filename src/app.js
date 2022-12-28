eab386ed695613633f8fdf3df2e80e784b16d987
nadia-bayat <121291038+nadia-bayat@users.noreply.github.com>
2022-12-28 15:52:43 +0330

    Updated time

--- src/app.js
+++ src/app.js
@@ -1,18 +1,6 @@
-function displayTemperature(response) {
-  console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let description = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let wind = document.querySelector("#wind");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  description.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  wind.innerHTML = Math.round(response.data.wind.speed);
-}
 let dateTime = document.querySelector("#dateTime");
 let now = new Date();
+
 let months = [
   "January",
   "February",
@@ -30,9 +18,26 @@ let months = [
 let month = months[now.getMonth()];
 let date = now.getDate();
 let hour = now.getHours();
+if (hour < 10) {
+  minute = `0${hour}`;
+}
 let minute = now.getMinutes();
-dateTime.innerHTML = `${month} ${date}, ${hour}:${minute}`;
-
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  dateTime.innerHTML = `${month} ${date}, ${hour}:${minute}`;
+}
 let apiKey = "c651b570409fd5da2a6ffe01cfd48b43";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Rome&appid=${apiKey}&units=metric`;
 axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemperature);
