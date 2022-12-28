622ad1b61ec73dd94cbcd81858aa07dfef2b40c9
nadia-bayat <121291038+nadia-bayat@users.noreply.github.com>
2022-12-28 16:21:21 +0330

    Updated the icon

--- src/app.js
+++ src/app.js
@@ -31,12 +31,18 @@ function displayTemperature(response) {
   let description = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
   dateTime.innerHTML = `${month} ${date}, ${hour}:${minute}`;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "c651b570409fd5da2a6ffe01cfd48b43";
 let city = "Rome";
