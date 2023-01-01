2764b0c84264b7dc775ef893471131589355ddf2
nadia-bayat <121291038+nadia-bayat@users.noreply.github.com>
2023-01-01 13:53:14 +0330

    Added API to JS

--- src/app.js
+++ src/app.js
@@ -25,7 +25,8 @@ let minute = now.getMinutes();
 if (minute < 10) {
   minute = `0${minute}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -53,6 +54,12 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -71,6 +78,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "c651b570409fd5da2a6ffe01cfd48b43";
@@ -83,7 +91,7 @@ function handleSubmit(event) {
   let city = document.querySelector("#search-city").value;
   search(city);
 }
-displayForecast();
+
 search("Rome");
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
